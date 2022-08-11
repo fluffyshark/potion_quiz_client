2850fc39283fb6de6875fef68c6902a5fb451ee2
fluffyshark <robin.yjord1@gmail.com>
2022-08-11 18:43:02 +0200

    build2

--- src/TestSocket.js
+++ src/TestSocket.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import io from "socket.io-client"
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://potion-quiz-server.herokuapp.com/")
 
 function TestSocket() {
     const [room, setRoom] = useState("");
