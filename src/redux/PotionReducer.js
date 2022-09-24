13192df9d8fa12fbd434d5d23cf8f4e98ec11b67
fluffyshark <robin.yjord1@gmail.com>
2022-09-24 12:27:39 +0200

    Deployment - Changing socket.io origin to server

--- src/redux/PotionReducer.js
+++ src/redux/PotionReducer.js
@@ -2,8 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 import { PotionData } from "./PotionData.js"
 import io from "socket.io-client"
 
-//const socket = io.connect("https://potion-quiz-server.herokuapp.com/")
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://potion-quiz-server.herokuapp.com/")
+//const socket = io.connect("http://localhost:3001")
 
 let levelsCounter = [0,0,0]
 
