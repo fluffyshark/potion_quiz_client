58e92b187d41262d10d922ba52fd540ae0a766b7
fluffyshark <robin.yjord1@gmail.com>
2022-10-30 17:39:39 +0100

    Socket - Changed socket connection to live server-2

--- src/redux/PlayerSocketReducer.js
+++ src/redux/PlayerSocketReducer.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import io from "socket.io-client"
 
-//const socket = io.connect("https://server-potionquiz.herokuapp.com/")
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://server-potionquiz.herokuapp.com/")
+//const socket = io.connect("http://localhost:3001")
 
 export var PlayerData = {playerID: "", playerName: "", cards: [0,0,0], gameCode: "", displayCode: 0}
 
