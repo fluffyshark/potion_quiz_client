1c9bc8ad691c34ae092535bb2d9bdeb28df81a29
fluffyshark <robin.yjord1@gmail.com>
2022-11-01 05:19:40 +0100

    Performance - Replaced .png herb golden images, (size totaled around 1.6mbit), with .webp (size totaling around 800kb)

--- src/image_assets/HerbImageGoldExport.js
+++ src/image_assets/HerbImageGoldExport.js
@@ -1,110 +1,110 @@
-import hg1 from "./herbs/golden/ingred1_g.png"; import hg6 from "./herbs/golden/ingred6_g.png";
-import hg2 from "./herbs/golden/ingred2_g.png"; import hg7 from "./herbs/golden/ingred7_g.png";
-import hg3 from "./herbs/golden/ingred3_g.png"; import hg8 from "./herbs/golden/ingred8_g.png";
-import hg4 from "./herbs/golden/ingred4_g.png"; import hg9 from "./herbs/golden/ingred9_g.png";
-import hg5 from "./herbs/golden/ingred5_g.png"; import hg10 from "./herbs/golden/ingred10_g.png";
+import hg1 from "./herbs/golden/ingred1_g.webp"; import hg6 from "./herbs/golden/ingred6_g.webp";
+import hg2 from "./herbs/golden/ingred2_g.webp"; import hg7 from "./herbs/golden/ingred7_g.webp";
+import hg3 from "./herbs/golden/ingred3_g.webp"; import hg8 from "./herbs/golden/ingred8_g.webp";
+import hg4 from "./herbs/golden/ingred4_g.webp"; import hg9 from "./herbs/golden/ingred9_g.webp";
+import hg5 from "./herbs/golden/ingred5_g.webp"; import hg10 from "./herbs/golden/ingred10_g.webp";
 
-import hg11 from "./herbs/golden/ingred11_g.png"; import hg16 from "./herbs/golden/ingred16_g.png";
-import hg12 from "./herbs/golden/ingred12_g.png"; import hg17 from "./herbs/golden/ingred17_g.png";
-import hg13 from "./herbs/golden/ingred13_g.png"; import hg18 from "./herbs/golden/ingred18_g.png";
-import hg14 from "./herbs/golden/ingred14_g.png"; import hg19 from "./herbs/golden/ingred19_g.png";
-import hg15 from "./herbs/golden/ingred15_g.png"; import hg20 from "./herbs/golden/ingred20_g.png";
+import hg11 from "./herbs/golden/ingred11_g.webp"; import hg16 from "./herbs/golden/ingred16_g.webp";
+import hg12 from "./herbs/golden/ingred12_g.webp"; import hg17 from "./herbs/golden/ingred17_g.webp";
+import hg13 from "./herbs/golden/ingred13_g.webp"; import hg18 from "./herbs/golden/ingred18_g.webp";
+import hg14 from "./herbs/golden/ingred14_g.webp"; import hg19 from "./herbs/golden/ingred19_g.webp";
+import hg15 from "./herbs/golden/ingred15_g.webp"; import hg20 from "./herbs/golden/ingred20_g.webp";
 
-import hg21 from "./herbs/golden/ingred21_g.png"; import hg26 from "./herbs/golden/ingred26_g.png";
-import hg22 from "./herbs/golden/ingred22_g.png"; import hg27 from "./herbs/golden/ingred27_g.png";
-import hg23 from "./herbs/golden/ingred23_g.png"; import hg28 from "./herbs/golden/ingred28_g.png";
-import hg24 from "./herbs/golden/ingred24_g.png"; import hg29 from "./herbs/golden/ingred29_g.png";
-import hg25 from "./herbs/golden/ingred25_g.png"; import hg30 from "./herbs/golden/ingred30_g.png";
+import hg21 from "./herbs/golden/ingred21_g.webp"; import hg26 from "./herbs/golden/ingred26_g.webp";
+import hg22 from "./herbs/golden/ingred22_g.webp"; import hg27 from "./herbs/golden/ingred27_g.webp";
+import hg23 from "./herbs/golden/ingred23_g.webp"; import hg28 from "./herbs/golden/ingred28_g.webp";
+import hg24 from "./herbs/golden/ingred24_g.webp"; import hg29 from "./herbs/golden/ingred29_g.webp";
+import hg25 from "./herbs/golden/ingred25_g.webp"; import hg30 from "./herbs/golden/ingred30_g.webp";
 
-import hg31 from "./herbs/golden/ingred31_g.png"; import hg36 from "./herbs/golden/ingred36_g.png";
-import hg32 from "./herbs/golden/ingred32_g.png"; import hg37 from "./herbs/golden/ingred37_g.png";
-import hg33 from "./herbs/golden/ingred33_g.png"; import hg38 from "./herbs/golden/ingred38_g.png";
-import hg34 from "./herbs/golden/ingred34_g.png"; import hg39 from "./herbs/golden/ingred39_g.png";
-import hg35 from "./herbs/golden/ingred35_g.png"; import hg40 from "./herbs/golden/ingred40_g.png";
+import hg31 from "./herbs/golden/ingred31_g.webp"; import hg36 from "./herbs/golden/ingred36_g.webp";
+import hg32 from "./herbs/golden/ingred32_g.webp"; import hg37 from "./herbs/golden/ingred37_g.webp";
+import hg33 from "./herbs/golden/ingred33_g.webp"; import hg38 from "./herbs/golden/ingred38_g.webp";
+import hg34 from "./herbs/golden/ingred34_g.webp"; import hg39 from "./herbs/golden/ingred39_g.webp";
+import hg35 from "./herbs/golden/ingred35_g.webp"; import hg40 from "./herbs/golden/ingred40_g.webp";
 
-import hg41 from "./herbs/golden/ingred41_g.png"; import hg46 from "./herbs/golden/ingred46_g.png";
-import hg42 from "./herbs/golden/ingred42_g.png"; import hg47 from "./herbs/golden/ingred47_g.png";
-import hg43 from "./herbs/golden/ingred43_g.png"; import hg48 from "./herbs/golden/ingred48_g.png";
-import hg44 from "./herbs/golden/ingred44_g.png"; import hg49 from "./herbs/golden/ingred49_g.png";
-import hg45 from "./herbs/golden/ingred45_g.png"; import hg50 from "./herbs/golden/ingred50_g.png";
+import hg41 from "./herbs/golden/ingred41_g.webp"; import hg46 from "./herbs/golden/ingred46_g.webp";
+import hg42 from "./herbs/golden/ingred42_g.webp"; import hg47 from "./herbs/golden/ingred47_g.webp";
+import hg43 from "./herbs/golden/ingred43_g.webp"; import hg48 from "./herbs/golden/ingred48_g.webp";
+import hg44 from "./herbs/golden/ingred44_g.webp"; import hg49 from "./herbs/golden/ingred49_g.webp";
+import hg45 from "./herbs/golden/ingred45_g.webp"; import hg50 from "./herbs/golden/ingred50_g.webp";
 
-import hg51 from "./herbs/golden/ingred51_g.png"; import hg56 from "./herbs/golden/ingred56_g.png";
-import hg52 from "./herbs/golden/ingred52_g.png"; import hg57 from "./herbs/golden/ingred57_g.png";
-import hg53 from "./herbs/golden/ingred53_g.png"; import hg58 from "./herbs/golden/ingred58_g.png";
-import hg54 from "./herbs/golden/ingred54_g.png"; import hg59 from "./herbs/golden/ingred59_g.png";
-import hg55 from "./herbs/golden/ingred55_g.png"; import hg60 from "./herbs/golden/ingred60_g.png";
+import hg51 from "./herbs/golden/ingred51_g.webp"; import hg56 from "./herbs/golden/ingred56_g.webp";
+import hg52 from "./herbs/golden/ingred52_g.webp"; import hg57 from "./herbs/golden/ingred57_g.webp";
+import hg53 from "./herbs/golden/ingred53_g.webp"; import hg58 from "./herbs/golden/ingred58_g.webp";
+import hg54 from "./herbs/golden/ingred54_g.webp"; import hg59 from "./herbs/golden/ingred59_g.webp";
+import hg55 from "./herbs/golden/ingred55_g.webp"; import hg60 from "./herbs/golden/ingred60_g.webp";
 
-import hg61 from "./herbs/golden/ingred61_g.png"; import hg66 from "./herbs/golden/ingred66_g.png";
-import hg62 from "./herbs/golden/ingred62_g.png"; import hg67 from "./herbs/golden/ingred67_g.png";
-import hg63 from "./herbs/golden/ingred63_g.png"; import hg68 from "./herbs/golden/ingred68_g.png";
-import hg64 from "./herbs/golden/ingred64_g.png"; import hg69 from "./herbs/golden/ingred69_g.png";
-import hg65 from "./herbs/golden/ingred65_g.png"; import hg70 from "./herbs/golden/ingred70_g.png";
+import hg61 from "./herbs/golden/ingred61_g.webp"; import hg66 from "./herbs/golden/ingred66_g.webp";
+import hg62 from "./herbs/golden/ingred62_g.webp"; import hg67 from "./herbs/golden/ingred67_g.webp";
+import hg63 from "./herbs/golden/ingred63_g.webp"; import hg68 from "./herbs/golden/ingred68_g.webp";
+import hg64 from "./herbs/golden/ingred64_g.webp"; import hg69 from "./herbs/golden/ingred69_g.webp";
+import hg65 from "./herbs/golden/ingred65_g.webp"; import hg70 from "./herbs/golden/ingred70_g.webp";
 
-import hg71 from "./herbs/golden/ingred71_g.png"; import hg76 from "./herbs/golden/ingred76_g.png";
-import hg72 from "./herbs/golden/ingred72_g.png"; import hg77 from "./herbs/golden/ingred77_g.png";
-import hg73 from "./herbs/golden/ingred73_g.png"; import hg78 from "./herbs/golden/ingred78_g.png";
-import hg74 from "./herbs/golden/ingred74_g.png"; import hg79 from "./herbs/golden/ingred79_g.png";
-import hg75 from "./herbs/golden/ingred75_g.png"; import hg80 from "./herbs/golden/ingred80_g.png";
+import hg71 from "./herbs/golden/ingred71_g.webp"; import hg76 from "./herbs/golden/ingred76_g.webp";
+import hg72 from "./herbs/golden/ingred72_g.webp"; import hg77 from "./herbs/golden/ingred77_g.webp";
+import hg73 from "./herbs/golden/ingred73_g.webp"; import hg78 from "./herbs/golden/ingred78_g.webp";
+import hg74 from "./herbs/golden/ingred74_g.webp"; import hg79 from "./herbs/golden/ingred79_g.webp";
+import hg75 from "./herbs/golden/ingred75_g.webp"; import hg80 from "./herbs/golden/ingred80_g.webp";
 
-import hg81 from "./herbs/golden/ingred81_g.png"; import hg86 from "./herbs/golden/ingred86_g.png";
-import hg82 from "./herbs/golden/ingred82_g.png"; import hg87 from "./herbs/golden/ingred87_g.png";
-import hg83 from "./herbs/golden/ingred83_g.png"; import hg88 from "./herbs/golden/ingred88_g.png";
-import hg84 from "./herbs/golden/ingred84_g.png"; import hg89 from "./herbs/golden/ingred89_g.png";
-import hg85 from "./herbs/golden/ingred85_g.png"; import hg90 from "./herbs/golden/ingred90_g.png";
+import hg81 from "./herbs/golden/ingred81_g.webp"; import hg86 from "./herbs/golden/ingred86_g.webp";
+import hg82 from "./herbs/golden/ingred82_g.webp"; import hg87 from "./herbs/golden/ingred87_g.webp";
+import hg83 from "./herbs/golden/ingred83_g.webp"; import hg88 from "./herbs/golden/ingred88_g.webp";
+import hg84 from "./herbs/golden/ingred84_g.webp"; import hg89 from "./herbs/golden/ingred89_g.webp";
+import hg85 from "./herbs/golden/ingred85_g.webp"; import hg90 from "./herbs/golden/ingred90_g.webp";
 
-import hg91 from "./herbs/golden/ingred91_g.png"; import hg96 from "./herbs/golden/ingred96_g.png";
-import hg92 from "./herbs/golden/ingred92_g.png"; import hg97 from "./herbs/golden/ingred97_g.png";
-import hg93 from "./herbs/golden/ingred93_g.png"; import hg98 from "./herbs/golden/ingred98_g.png";
-import hg94 from "./herbs/golden/ingred94_g.png"; import hg99 from "./herbs/golden/ingred99_g.png";
-import hg95 from "./herbs/golden/ingred95_g.png"; import hg100 from "./herbs/golden/ingred100_g.png";
+import hg91 from "./herbs/golden/ingred91_g.webp"; import hg96 from "./herbs/golden/ingred96_g.webp";
+import hg92 from "./herbs/golden/ingred92_g.webp"; import hg97 from "./herbs/golden/ingred97_g.webp";
+import hg93 from "./herbs/golden/ingred93_g.webp"; import hg98 from "./herbs/golden/ingred98_g.webp";
+import hg94 from "./herbs/golden/ingred94_g.webp"; import hg99 from "./herbs/golden/ingred99_g.webp";
+import hg95 from "./herbs/golden/ingred95_g.webp"; import hg100 from "./herbs/golden/ingred100_g.webp";
 
-import hg101 from "./herbs/golden/ingred101_g.png"; import hg106 from "./herbs/golden/ingred106_g.png";
-import hg102 from "./herbs/golden/ingred102_g.png"; import hg107 from "./herbs/golden/ingred107_g.png";
-import hg103 from "./herbs/golden/ingred103_g.png"; import hg108 from "./herbs/golden/ingred108_g.png";
-import hg104 from "./herbs/golden/ingred104_g.png"; import hg109 from "./herbs/golden/ingred109_g.png";
-import hg105 from "./herbs/golden/ingred105_g.png"; import hg110 from "./herbs/golden/ingred110_g.png";
+import hg101 from "./herbs/golden/ingred101_g.webp"; import hg106 from "./herbs/golden/ingred106_g.webp";
+import hg102 from "./herbs/golden/ingred102_g.webp"; import hg107 from "./herbs/golden/ingred107_g.webp";
+import hg103 from "./herbs/golden/ingred103_g.webp"; import hg108 from "./herbs/golden/ingred108_g.webp";
+import hg104 from "./herbs/golden/ingred104_g.webp"; import hg109 from "./herbs/golden/ingred109_g.webp";
+import hg105 from "./herbs/golden/ingred105_g.webp"; import hg110 from "./herbs/golden/ingred110_g.webp";
 
-import hg111 from "./herbs/golden/ingred111_g.png"; import hg116 from "./herbs/golden/ingred116_g.png";
-import hg112 from "./herbs/golden/ingred112_g.png"; import hg117 from "./herbs/golden/ingred117_g.png";
-import hg113 from "./herbs/golden/ingred113_g.png"; import hg118 from "./herbs/golden/ingred118_g.png";
-import hg114 from "./herbs/golden/ingred114_g.png"; import hg119 from "./herbs/golden/ingred119_g.png";
-import hg115 from "./herbs/golden/ingred115_g.png"; import hg120 from "./herbs/golden/ingred120_g.png";
+import hg111 from "./herbs/golden/ingred111_g.webp"; import hg116 from "./herbs/golden/ingred116_g.webp";
+import hg112 from "./herbs/golden/ingred112_g.webp"; import hg117 from "./herbs/golden/ingred117_g.webp";
+import hg113 from "./herbs/golden/ingred113_g.webp"; import hg118 from "./herbs/golden/ingred118_g.webp";
+import hg114 from "./herbs/golden/ingred114_g.webp"; import hg119 from "./herbs/golden/ingred119_g.webp";
+import hg115 from "./herbs/golden/ingred115_g.webp"; import hg120 from "./herbs/golden/ingred120_g.webp";
 
-import hg121 from "./herbs/golden/ingred121_g.png"; import hg126 from "./herbs/golden/ingred126_g.png";
-import hg122 from "./herbs/golden/ingred122_g.png"; import hg127 from "./herbs/golden/ingred127_g.png";
-import hg123 from "./herbs/golden/ingred123_g.png"; import hg128 from "./herbs/golden/ingred128_g.png";
-import hg124 from "./herbs/golden/ingred124_g.png"; import hg129 from "./herbs/golden/ingred129_g.png";
-import hg125 from "./herbs/golden/ingred125_g.png"; import hg130 from "./herbs/golden/ingred130_g.png";
+import hg121 from "./herbs/golden/ingred121_g.webp"; import hg126 from "./herbs/golden/ingred126_g.webp";
+import hg122 from "./herbs/golden/ingred122_g.webp"; import hg127 from "./herbs/golden/ingred127_g.webp";
+import hg123 from "./herbs/golden/ingred123_g.webp"; import hg128 from "./herbs/golden/ingred128_g.webp";
+import hg124 from "./herbs/golden/ingred124_g.webp"; import hg129 from "./herbs/golden/ingred129_g.webp";
+import hg125 from "./herbs/golden/ingred125_g.webp"; import hg130 from "./herbs/golden/ingred130_g.webp";
 
-import hg131 from "./herbs/golden/ingred131_g.png"; import hg136 from "./herbs/golden/ingred136_g.png";
-import hg132 from "./herbs/golden/ingred132_g.png"; import hg137 from "./herbs/golden/ingred137_g.png";
-import hg133 from "./herbs/golden/ingred133_g.png"; import hg138 from "./herbs/golden/ingred138_g.png";
-import hg134 from "./herbs/golden/ingred134_g.png"; import hg139 from "./herbs/golden/ingred139_g.png";
-import hg135 from "./herbs/golden/ingred135_g.png"; import hg140 from "./herbs/golden/ingred140_g.png";
+import hg131 from "./herbs/golden/ingred131_g.webp"; import hg136 from "./herbs/golden/ingred136_g.webp";
+import hg132 from "./herbs/golden/ingred132_g.webp"; import hg137 from "./herbs/golden/ingred137_g.webp";
+import hg133 from "./herbs/golden/ingred133_g.webp"; import hg138 from "./herbs/golden/ingred138_g.webp";
+import hg134 from "./herbs/golden/ingred134_g.webp"; import hg139 from "./herbs/golden/ingred139_g.webp";
+import hg135 from "./herbs/golden/ingred135_g.webp"; import hg140 from "./herbs/golden/ingred140_g.webp";
 
-import hg141 from "./herbs/golden/ingred141_g.png"; import hg146 from "./herbs/golden/ingred146_g.png";
-import hg142 from "./herbs/golden/ingred142_g.png"; import hg147 from "./herbs/golden/ingred147_g.png";
-import hg143 from "./herbs/golden/ingred143_g.png"; import hg148 from "./herbs/golden/ingred148_g.png";
-import hg144 from "./herbs/golden/ingred144_g.png"; import hg149 from "./herbs/golden/ingred149_g.png";
-import hg145 from "./herbs/golden/ingred145_g.png"; import hg150 from "./herbs/golden/ingred150_g.png";
+import hg141 from "./herbs/golden/ingred141_g.webp"; import hg146 from "./herbs/golden/ingred146_g.webp";
+import hg142 from "./herbs/golden/ingred142_g.webp"; import hg147 from "./herbs/golden/ingred147_g.webp";
+import hg143 from "./herbs/golden/ingred143_g.webp"; import hg148 from "./herbs/golden/ingred148_g.webp";
+import hg144 from "./herbs/golden/ingred144_g.webp"; import hg149 from "./herbs/golden/ingred149_g.webp";
+import hg145 from "./herbs/golden/ingred145_g.webp"; import hg150 from "./herbs/golden/ingred150_g.webp";
 
-import hg151 from "./herbs/golden/ingred151_g.png"; import hg156 from "./herbs/golden/ingred156_g.png";
-import hg152 from "./herbs/golden/ingred152_g.png"; import hg157 from "./herbs/golden/ingred157_g.png";
-import hg153 from "./herbs/golden/ingred153_g.png"; import hg158 from "./herbs/golden/ingred158_g.png";
-import hg154 from "./herbs/golden/ingred154_g.png"; import hg159 from "./herbs/golden/ingred159_g.png";
-import hg155 from "./herbs/golden/ingred155_g.png"; import hg160 from "./herbs/golden/ingred160_g.png";
+import hg151 from "./herbs/golden/ingred151_g.webp"; import hg156 from "./herbs/golden/ingred156_g.webp";
+import hg152 from "./herbs/golden/ingred152_g.webp"; import hg157 from "./herbs/golden/ingred157_g.webp";
+import hg153 from "./herbs/golden/ingred153_g.webp"; import hg158 from "./herbs/golden/ingred158_g.webp";
+import hg154 from "./herbs/golden/ingred154_g.webp"; import hg159 from "./herbs/golden/ingred159_g.webp";
+import hg155 from "./herbs/golden/ingred155_g.webp"; import hg160 from "./herbs/golden/ingred160_g.webp";
 
-import hg161 from "./herbs/golden/ingred161_g.png"; import hg166 from "./herbs/golden/ingred166_g.png";
-import hg162 from "./herbs/golden/ingred162_g.png"; import hg167 from "./herbs/golden/ingred167_g.png";
-import hg163 from "./herbs/golden/ingred163_g.png"; import hg168 from "./herbs/golden/ingred168_g.png";
-import hg164 from "./herbs/golden/ingred164_g.png"; import hg169 from "./herbs/golden/ingred169_g.png";
-import hg165 from "./herbs/golden/ingred165_g.png"; import hg170 from "./herbs/golden/ingred170_g.png";
+import hg161 from "./herbs/golden/ingred161_g.webp"; import hg166 from "./herbs/golden/ingred166_g.webp";
+import hg162 from "./herbs/golden/ingred162_g.webp"; import hg167 from "./herbs/golden/ingred167_g.webp";
+import hg163 from "./herbs/golden/ingred163_g.webp"; import hg168 from "./herbs/golden/ingred168_g.webp";
+import hg164 from "./herbs/golden/ingred164_g.webp"; import hg169 from "./herbs/golden/ingred169_g.webp";
+import hg165 from "./herbs/golden/ingred165_g.webp"; import hg170 from "./herbs/golden/ingred170_g.webp";
 
-import hg171 from "./herbs/golden/ingred171_g.png"; import hg176 from "./herbs/golden/ingred176_g.png";
-import hg172 from "./herbs/golden/ingred172_g.png"; import hg177 from "./herbs/golden/ingred177_g.png";
-import hg173 from "./herbs/golden/ingred173_g.png"; import hg178 from "./herbs/golden/ingred178_g.png";
-import hg174 from "./herbs/golden/ingred174_g.png"; import hg179 from "./herbs/golden/ingred179_g.png";
-import hg175 from "./herbs/golden/ingred175_g.png"; import hg180 from "./herbs/golden/ingred180_g.png";
+import hg171 from "./herbs/golden/ingred171_g.webp"; import hg176 from "./herbs/golden/ingred176_g.webp";
+import hg172 from "./herbs/golden/ingred172_g.webp"; import hg177 from "./herbs/golden/ingred177_g.webp";
+import hg173 from "./herbs/golden/ingred173_g.webp"; import hg178 from "./herbs/golden/ingred178_g.webp";
+import hg174 from "./herbs/golden/ingred174_g.webp"; import hg179 from "./herbs/golden/ingred179_g.webp";
+import hg175 from "./herbs/golden/ingred175_g.webp"; import hg180 from "./herbs/golden/ingred180_g.webp";
 
 const goldenHerbs = [
     hg1,hg2,hg3,hg4,hg5,hg6,hg7,hg8,hg9,hg10,
