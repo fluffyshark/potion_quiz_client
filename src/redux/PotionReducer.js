de626f7f0da3f642ed4ee6b6342a44d2de60f5fd
fluffyshark <robin.yjord1@gmail.com>
2022-11-20 10:43:21 +0100

    Socket - Experimenting with Cors settings v5.2

--- src/redux/PotionReducer.js
+++ src/redux/PotionReducer.js
@@ -2,8 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 import { PotionData } from "./PotionData.js"
 import io from "socket.io-client"
 
-//const socket = io.connect("https://potionquiz.com/")
-const socket = io.connect("http://16.171.11.140/")
+const socket = io.connect("https://potionquiz.com/")
+//const socket = io.connect("http://16.171.11.140/")
 //const socket = io.connect("https://server-potionquiz.herokuapp.com/")
 //const socket = io.connect("http://localhost:3001")
 
