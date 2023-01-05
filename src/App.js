9cd4a1c59964c2dd84ff610d6678d82ce2b36202
fluffyshark <robin.yjord1@gmail.com>
2023-01-05 17:46:46 +0100

    Sound - Testing to implement sound effects for buttons-2

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ const HostingView = lazy(() => import("./pages/hostingView/HostingView"))
 const JoinView = lazy(() => import("./pages/joinView/JoinView"))
 const PotionAuction = lazy(() => import("./pages/potionAuction/PotionAuction"))
 
-//const socket = io.connect("https://potionquiz.com/")
+const socket = io.connect("https://potionquiz.com/")
 //const socket = io.connect("http://16.171.11.140/")
 //const socket = io.connect("https://server-potionquiz.herokuapp.com/")
-const socket = io.connect("http://localhost:3001")
+//const socket = io.connect("http://localhost:3001")
 
 let ourHostID = ""
 
